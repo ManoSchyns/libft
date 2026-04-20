0f23fa25285417cd26265ac1837ef5e2c36e1858
mschyns mschyns <mschyns@f6r13s15.paris.42.school>
2026-04-20 11:46:46 +0200

    correction du prototype de calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,28 +6,28 @@
 /*   By: mschyns <mano.schyns@learner.42.tech>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 10:55:03 by mschyns           #+#    #+#             */
-/*   Updated: 2026/04/20 10:55:04 by mschyns          ###   ########.fr       */
+/*   Updated: 2026/04/20 11:46:05 by mschyns          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t n, size_t size)
 {
 	void			*dest;
 	unsigned char	*work;
 	size_t			i;
 
-	if (nmemb != 0 && size > SIZE_MAX / nmemb)
+	if (n != 0 && size > SIZE_MAX / n)
 		return (NULL);
-	if (nmemb == 0 || size == 0)
+	if (n == 0 || size == 0)
 		return (malloc(1));
 	i = 0;
-	dest = malloc(nmemb * size);
+	dest = malloc(n * size);
 	if (dest == NULL)
 		return (NULL);
 	work = (unsigned char *)dest;
-	while (i < nmemb * size)
+	while (i < n * size)
 	{
 		work[i] = 0;
 		i ++;
